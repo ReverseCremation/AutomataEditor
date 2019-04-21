2e622188e1f5c08b7e09d71587688b335349f68c
ReverseCremation <35652220+ReverseCremation@users.noreply.github.com>
2019-04-21 12:32:04 -0500

    world class started
    Do research to figure out how to construct the world. Options to consider:
        // 1. Array of pointers to arrays (or vector of pointers to vectors?)
        // 2. Vector of vectors
        // 3. Matrix class
        // 4. 2D array
        // 5. Linearize a 1D array/vector - a_i = row_number*MAX_row_length + col_number (where col_number <= MAX_col_number)

--- world.h
+++ world.h
@@ -2,14 +2,21 @@
 #define WORLD_H
 
 #include "automata.h"
+#include <vector>
 
 class world{
 
 private:
-    // World in rectangular and defined by horizontal length and vertical width
+    // World is rectangular and defined by horizontal length and vertical width
     unsigned int length;
     unsigned int width;
-    
+
+    // Possible ways to create world:
+    // 1. Array of pointers to arrays (or vector of pointers to vectors?)
+    // 2. Vector of vectors
+    // 3. Matrix class
+    // 4. 2D array
+    // 5. Linearize a 1D array/vector - a_i = row_number*MAX_row_length + col_number (where col_number <= MAX_col_number)
 };
 
 
